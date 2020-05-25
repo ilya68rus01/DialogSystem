6fea536088413a9798d114e963a0b01e2e0fc8a9
Ilya <ilya68rus01@gmail.com>
2020-05-25 17:23:38 +0300

    Backup

--- DialogService/app.py
+++ DialogService/app.py
@@ -1,3 +1,7 @@
 
-print("Hello, World!")
-print("Сделал git commit -am \'text commit\' и git push origin master")
+class Test:
+    def __init__(self, text):
+        self.text_value = str(text) + " It's work!"
+
+    def get_text(self):
+        return self.text_value
