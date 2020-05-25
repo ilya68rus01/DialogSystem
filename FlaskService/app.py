0f23e6f693dcd0dbf8579f342087f8014906fd59
Ilya <ilya68rus01@gmail.com>
2020-05-25 17:24:46 +0300

    Backup v2

---
+++ FlaskService/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask
+from DialogService.app import Test
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    test = Test("Hello, World!")
+    return test.get_text()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
